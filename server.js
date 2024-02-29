364b8307aea36417d14f8181e6843da1364810d2
YusukeSakuraba <yusuke.sakuraba0213@gmail.com>
2024-02-29 20:48:00 +0900

    add server

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express")
+const app = express();
+const port = 3000;
+
+app.get("/",(req,res,next)=>{
+    res.end("top page");
+});
+
+// サーバーを起動する
+app.listen(port, ()=>{
+    console.log(`server is running on http://localhost:${port}`)
+});
