d1780f24fc5941aa3a6445437c797a559ef3d75a
YusukeSakuraba <yusuke.sakuraba0213@gmail.com>
2024-02-29 21:16:36 +0900

    add post routes

--- routes/api.js
+++ routes/api.js
@@ -5,4 +5,16 @@ router.get("/",(req,res)=>{
     res.send({mesage:"Hello"});
 });
 
+router.post("/",(req,res)=>{
+    const body = req.body;
+    console.log(body);
+    res.end();
+});
+
 module.exports = router;
+
+// await fetch("http://localhost:3000/api/",{
+//     method: "POST",
+//     body:JSON.stringify({message:"こんにちは"}),
+//     headers: {"Content-type": "applciation/json"}
+// });
