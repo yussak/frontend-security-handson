58a1e286bba4076809bce051c7fcc5cb37d76f9a
YusukeSakuraba <yusuke.sakuraba0213@gmail.com>
2024-02-29 20:50:03 +0900

    use public/index.html

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
-const express = require("express")
+const express = require("express");
 const app = express();
 const port = 3000;
+
+app.use(express.static("public"));
 
 app.get("/",(req,res,next)=>{
     res.end("top page");
