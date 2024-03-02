001a0823c28aed1fe378cac04110fa5c254fc10a
YusukeSakuraba <yusuke.sakuraba0213@gmail.com>
2024-03-02 11:18:08 +0900

    change error message by x-lang

--- routes/api.js
+++ routes/api.js
@@ -2,11 +2,17 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/",(req,res)=>{
+    res.setHeader("X-Timestamp", Date.now());
     let message = req.query.message;
+    const lang = req.headers["x-lang"];
 
     if (message === "") {
         res.status(400);
-        message ="messageが空です";
+        if (lang === "en") {
+            message = "message is empty";
+        } else {
+            message ="messageが空です";
+        }
     }
 
     res.send({mesage: message});
@@ -26,3 +32,8 @@ module.exports = router;
 //     body:JSON.stringify({message:"こんにちは"}),
 //     headers: {"Content-type": "applciation/json"}
 // });
+
+// const res = await fetch("http://localhost:3000/api/?message=", {
+//     headers: {"X-lang": "en"},
+// });
+// await res.json()
